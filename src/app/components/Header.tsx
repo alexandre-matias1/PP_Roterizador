e6e64bfc2f2956bcd10e3c71276c443a6ebb5d64
Alexandre Matias <axdmatias310@gmail.com>
2025-05-30 08:46:15 -0300

    change header style

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -7,11 +7,11 @@ export function Header() {
       <div className="p-6 flex gap-8 items-center bg-zinc-950 border-b-1 border-zinc-300">
         <Truck color="#f1f1f1" size={32}/>
         <span className="text-zinc-50 font-sans text-xl">Roteirizador</span>
-        <div className="flex gap-8 absolute z-1 top-13 left-190">
+        <div className="flex gap-8 absolute z-1 top-2 left-185">
           <button className="bg-red-800 px-4 rounded-2xl h-14  text-zinc-50 border-zinc-200 border hover:bg-red-700"
           onClick={()=>{toast.success("Rota recalculada")}}>Recalcular</button>
           <button className="bg-cyan-800 px-4 rounded-2xl h-14  text-zinc-50 border-zinc-200 border hover:bg-cyan-700"
-          onClick={()=>{toast.success("Rota recalculada")}}>Enviar para o protheus</button>
+          onClick={()=>{toast.success("Dados enviados para o protheus")}}>Enviar para o protheus</button>
         </div>
       </div>
       
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,17 +7,15 @@ import { LeftSide } from "./components/Left-Side";
 export default function Page() {
   return (
     <>
-      <div className="overflow-y-auto h-svh ">
-        <Header />
         <div className="flex h-svh overflow-y-auto">
           <div className="w-200 bg-zinc-950 flex flex-col overflow-y-auto">
+            <Header />
             <div className="flex flex-col  mt-5 ">
               <LeftSide />
             </div>
           </div>
           <Map />
         </div>
-      </div>
     </>
   );
 }
